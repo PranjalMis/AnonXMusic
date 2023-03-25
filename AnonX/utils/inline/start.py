1992ac2cc9217ea48e0f22e1a37789e3df304aa3
Fᴀʙɪɴᴏ <116137433+FabinoXD@users.noreply.github.com>
2023-03-25 11:24:19 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍɪsᴀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text="ᴀᴅᴅ ʜᴏɴᴇʏ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
